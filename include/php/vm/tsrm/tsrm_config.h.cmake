5e801c81e76194bd4d22ddd28db7f31a4dbad479
zzusoftboy <zzu_softboy@163.com>
2018-08-26 01:19:36 +0800

    add include directory

---
+++ include/php/vm/tsrm/tsrm_config.h.cmake
@@ -0,0 +1,12 @@
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2018 polarPHP software foundation
+// Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://polarphp.org/LICENSE.txt for license information
+// See http://polarphp.org/CONTRIBUTORS.txt for the list of polarPHP project authors
+//
+// Created by polarboy on 2018/08/25.
+
+#include "../../global/php_config.h"
