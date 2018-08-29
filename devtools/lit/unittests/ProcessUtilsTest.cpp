c84b2966e38a194573e1c46930b8cfc54ca57e54
zzusoftboy <zzu_softboy@163.com>
2018-08-29 18:26:58 +0800

    fix thirdparty library build issue pass right compiler path

--- devtools/lit/unittests/ProcessUtilsTest.cpp
+++ devtools/lit/unittests/ProcessUtilsTest.cpp
@@ -1,5 +1,15 @@
-#include <gtest/gtest.h>
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2018 polarphp software foundation
+// Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://polarphp.org/LICENSE.txt for license information
+// See http://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2018/08/29.
 
+#include <gtest/gtest.h>
 
 TEST(ProcessUtilsTest, testBasic)
 {
