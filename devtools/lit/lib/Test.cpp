42aa8d0e328a1f6765a8c7d2d384612d2b1df2f6
zzusoftboy <zzu_softboy@163.com>
2018-09-06 22:36:04 +0800

    add Test::isExpectedToFail method

--- devtools/lit/lib/Test.cpp
+++ devtools/lit/lib/Test.cpp
@@ -11,6 +11,7 @@
 
 #include "Test.h"
 #include "Global.h"
+#include "BooleanExpression.h"
 
 namespace polar {
 namespace lit {
@@ -79,7 +80,25 @@ void Test::setResult(const Result &result)
 bool Test::isExpectedToFail()
 {
    const std::set<std::string> &features = m_config.getAvailableFeatures();
-
+   const std::string &triple = m_config.getExtraConfig<std::string>("target_triple", std::string(""));
+   // Check if any of the xfails match an available feature or the target.
+   for (const std::string &item : m_xfails) {
+      // If this is the wildcard, it always fails.
+      if (item == "*") {
+         return true;
+      }
+      // If this is a True expression of features and target triple parts,
+      // it fails.
+      try {
+         std::optional<bool> evalResult = BooleanExpression::evaluate(item, features, triple);
+         if (evalResult.has_value() && evalResult.value()) {
+            return true;
+         }
+      } catch (ValueError &error) {
+          throw ValueError(format_string("Error in XFAIL list:\n%s", error.what()));
+      }
+   }
+   return false;
 }
 
 } // lit
