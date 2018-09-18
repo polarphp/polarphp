f7dd307d0d597bf2e75416c21fbfaa8323417bbc
zzusoftboy <zzu_softboy@163.com>
2018-09-19 00:18:20 +0800

    implement builtin cat command for lit

--- devtools/lit/builtincommands/Cat.cpp
+++ devtools/lit/builtincommands/Cat.cpp
@@ -17,6 +17,42 @@
 #include <filesystem>
 #include <list>
 
+std::string convert_to_caret_and_mnotation(std::string data)
+{
+   std::string output;
+   for (char &c : data) {
+      if (c == 9 || c == 10) {
+         output.push_back(c);
+         continue;
+      }
+      if (c > 127) {
+         c = c - 128;
+         output += "M-";
+      }
+      if (c < 32) {
+         output.push_back('^');
+         output.push_back(c + 64);
+      } else if (c == 127) {
+         output += "^?";
+      } else {
+         output.push_back(c);
+      }
+   }
+   return output;
+}
+
+void general_exception_handler(std::exception_ptr eptr)
+{
+   try {
+      if (eptr) {
+         std::rethrow_exception(eptr);
+      }
+   } catch (const std::exception &exp) {
+      std::cerr << exp.what() << std::endl;
+      exit(1);
+   }
+}
+
 int main(int argc, char *argv[])
 {
    CLI::App catApp;
@@ -25,5 +61,27 @@ int main(int argc, char *argv[])
    catApp.add_option("-v, --show-nonprinting", showNonprinting, "show all non printable char", false);
    catApp.add_option("filenames", filenames, "Filenames to been print")->required();
    CLI11_PARSE(catApp, argc, argv);
+   std::exception_ptr eptr;
+   try {
+      char buffer[1024];
+      for (const std::string &filename : filenames) {
+         std::ifstream fstream(filename, std::ios_base::in | std::ios_base::binary);
+         if (!fstream.is_open()) {
+            throw std::runtime_error(std::string("open file ") + filename + " failure");
+         }
+         while (!fstream.eof()) {
+            fstream.read(buffer, 1024);
+            if (showNonprinting) {
+               std::cout << convert_to_caret_and_mnotation(std::string(buffer, fstream.gcount()));
+            } else {
+               std::cout << std::string(buffer, fstream.gcount());
+            }
+         }
+         std::cout.flush();
+      }
+   } catch (...) {
+      eptr = std::current_exception();
+   }
+   general_exception_handler(eptr);
    return 0;
 }
