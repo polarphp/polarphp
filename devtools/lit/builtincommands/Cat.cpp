1bb41ad3e064f532656cb0bb1dc443a9f881afda
zzusoftboy <zzu_softboy@163.com>
2018-09-18 22:46:01 +0800

    add Cat command line parse

--- devtools/lit/builtincommands/Cat.cpp
+++ devtools/lit/builtincommands/Cat.cpp
@@ -9,7 +9,21 @@
 //
 // Created by polarboy on 2018/09/18.
 
+#include "CLI/CLI.hpp"
+#include "Config.h"
+#include <iostream>
+#include <thread>
+#include <assert.h>
+#include <filesystem>
+#include <list>
+
 int main(int argc, char *argv[])
 {
-
+   CLI::App catApp;
+   bool showNonprinting = false;
+   std::vector<std::string> filenames;
+   catApp.add_option("-v, --show-nonprinting", showNonprinting, "show all non printable char", false);
+   catApp.add_option("filenames", filenames, "Filenames to been print")->required();
+   CLI11_PARSE(catApp, argc, argv);
+   return 0;
 }
