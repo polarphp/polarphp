7e189492a7feb41b328f7a334fba007abcdde397
zzusoftboy <zzu_softboy@163.com>
2018-10-02 21:57:08 +0800

    add semaphore class

---
+++ devtools/lit/lib/Semaphore.h
@@ -0,0 +1,58 @@
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2018 polarphp software foundation
+// Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://polarphp.org/LICENSE.txt for license information
+// See http://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2018/10/02.
+
+#ifndef POLAR_DEVLTOOLS_SEMAPHORE_H
+#define POLAR_DEVLTOOLS_SEMAPHORE_H
+
+#include <mutex>
+#include <condition_variable>
+
+namespace polar {
+namespace lit {
+
+class Semaphore
+{
+private:
+   std::mutex m_mutex;
+   std::condition_variable m_condition;
+   unsigned long m_count = 0; // Initialized as locked.
+
+public:
+   void notify()
+   {
+      std::lock_guard<decltype(m_mutex)> lock(m_mutex);
+      ++m_count;
+      m_condition.notify_one();
+   }
+
+   void wait()
+   {
+      std::unique_lock<decltype(m_mutex)> lock(m_mutex);
+      while(!m_count) // Handle spurious wake-ups.
+         m_condition.wait(lock);
+      --m_count;
+   }
+
+   bool try_wait()
+   {
+      std::lock_guard<decltype(m_mutex)> lock(m_mutex);
+      if(m_count) {
+         --m_count;
+         return true;
+      }
+      return false;
+   }
+};
+
+} // lit
+} // polar
+
+#endif // POLAR_DEVLTOOLS_SEMAPHORE_H
