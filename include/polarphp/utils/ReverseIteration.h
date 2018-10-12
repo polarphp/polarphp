4ef97be6990be7c44c2d69b9e00e33aafc3cd1d2
zzusoftboy <zzu_softboy@163.com>
2018-10-12 19:03:59 +0800

    add reverse iterator

---
+++ include/polarphp/utils/ReverseIteration.h
@@ -0,0 +1,35 @@
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2018 polarPHP software foundation
+// Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://polarphp.org/LICENSE.txt for license information
+// See http://polarphp.org/CONTRIBUTORS.txt for the list of polarPHP project authors
+//
+// Created by softboy on 2018/06/09.
+
+#ifndef POLAR_UTILS_REVERSE_ITERATION_H
+#define POLAR_UTILS_REVERSE_ITERATION_H
+
+#include "polarphp/global/Config.h"
+#include "polarphp/utils/PointerLikeTypeTraits.h"
+
+namespace polar {
+namespace utils {
+
+template<class T = void *>
+bool should_reverse_iterate()
+{
+#if POLAR_ENABLE_REVERSE_ITERATION
+  return internal::IsPointerLike<T>::value;
+#else
+  return false;
+#endif
+
+}
+
+} // utils
+} // polar
+
+#endif // POLAR_UTILS_REVERSE_ITERATION_H
