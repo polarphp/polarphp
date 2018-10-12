a5518337434e07f36c3bd077dcd6fd5f151926ee
zzusoftboy <zzu_softboy@163.com>
2018-10-12 17:29:53 +0800

    add Hashing

---
+++ include/polarphp/utils/EndianStream.h
@@ -0,0 +1,27 @@
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2018 polarphp software foundation
+// Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://polarphp.org/LICENSE.txt for license information
+// See http://polarphp.org/CONTRIBUTORS.txt for the list of polarPHP project authors
+//
+// Created by softboy on 2018/07/01.
+
+#ifndef POLARPHP_UTILS_ENDIAN_STREAM_H
+#define POLARPHP_UTILS_ENDIAN_STREAM_H
+
+//#include "polarphp/basic/adt/ArrayRef.h"
+//#include "polarphp/utils/Endian.h"
+//#include "polarphp/utils/RawOutStream.h"
+
+namespace polar {
+namespace utils {
+namespace endian {
+
+} // end
+} // utils
+} // polar
+
+#endif // POLARPHP_UTILS_ENDIAN_STREAM_H
