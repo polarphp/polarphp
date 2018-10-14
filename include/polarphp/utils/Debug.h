98b2bc0a1f3ad92b3af7ddda750473b034aa3237
zzusoftboy <zzu_softboy@163.com>
2018-10-14 22:13:18 +0800

    add DagDeltaAlgorithm class

--- include/polarphp/utils/Debug.h
+++ include/polarphp/utils/Debug.h
@@ -16,10 +16,91 @@
 
 namespace polar {
 
+#ifndef NDEBUG
+
+/// is_current_debug_type - Return true if the specified string is the debug type
+/// specified on the command line, or if none was specified on the command line
+/// with the -debug-only=X option.
+///
+bool is_current_debug_type(const char *Type);
+
+/// set_current_debug_type - Set the current debug type, as if the -debug-only=X
+/// option were specified.  Note that DebugFlag also needs to be set to true for
+/// debug output to be produced.
+///
+void set_current_debug_type(const char *Type);
+
+/// set_current_debug_types - Set the current debug type, as if the
+/// -debug-only=X,Y,Z option were specified. Note that DebugFlag
+/// also needs to be set to true for debug output to be produced.
+///
+void set_current_debug_types(const char **Types, unsigned Count);
+
+/// DEBUG_WITH_TYPE macro - This macro should be used by passes to emit debug
+/// information.  In the '-debug' option is specified on the commandline, and if
+/// this is a debug build, then the code specified as the option to the macro
+/// will be executed.  Otherwise it will not be.  Example:
+///
+/// DEBUG_WITH_TYPE("bitset", dbgs() << "Bitset contains: " << Bitset << "\n");
+///
+/// This will emit the debug information if -debug is present, and -debug-only
+/// is not specified, or is specified as "bitset".
+#define DEBUG_WITH_TYPE(TYPE, X)                                        \
+   do { if (::polar::DebugFlag && ::polar::is_current_debug_type(TYPE)) { X; } \
+} while (false)
+
+#else
+#define is_current_debug_type(X) (false)
+#define set_current_debug_type(X)
+#define set_current_debug_types(X, N)
+#define DEBUG_WITH_TYPE(TYPE, X) do { } while (false)
+#endif
+
+/// This boolean is set to true if the '-debug' command line option
+/// is specified.  This should probably not be referenced directly, instead, use
+/// the DEBUG macro below.
+///
+extern bool DebugFlag;
+
+/// \name Verification flags.
+///
+/// These flags turns on/off that are expensive and are turned off by default,
+/// unless macro EXPENSIVE_CHECKS is defined. The flags allow selectively
+/// turning the checks on without need to recompile.
+/// \{
+
+/// Enables verification of dominator trees.
+///
+extern bool VerifyDomInfo;
+
+/// Enables verification of loop info.
+///
+extern bool VerifyLoopInfo;
+
+///\}
+
+/// EnableDebugBuffering - This defaults to false.  If true, the debug
+/// stream will install signal handlers to dump any buffered debug
+/// output.  It allows clients to selectively allow the debug stream
+/// to install signal handlers if they are certain there will be no
+/// conflict.
+///
+extern bool EnableDebugBuffering;
+
+
 inline std::ostream &debug_stream()
 {
    return error_stream();
 }
+
+// DEBUG macro - This macro should be used by passes to emit debug information.
+// In the '-debug' option is specified on the commandline, and if this is a
+// debug build, then the code specified as the option to the macro will be
+// executed.  Otherwise it will not be.  Example:
+//
+// POLAR_DEBUG(dbgs() << "Bitset contains: " << Bitset << "\n");
+//
+#define POLAR_DEBUG(X) DEBUG_WITH_TYPE(DEBUG_TYPE, X)
 
 } // polar
 
