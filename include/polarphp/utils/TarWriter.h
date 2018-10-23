d320581e7845929583e8d76fe9b8726ad5f44ec3
zzusoftboy <zzu_softboy@163.com>
2018-10-23 22:50:31 +0800

    add ThreadPool

--- include/polarphp/utils/TarWriter.h
+++ include/polarphp/utils/TarWriter.h
@@ -1,11 +1,11 @@
 // This source file is part of the polarphp.org open source project
 //
-// Copyright (c) 2017 - 2018 POLARPHP_UTILS_SYSTEM_UTILS_H software foundation
+// Copyright (c) 2017 - 2018 polarphp software foundation
 // Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See http://polarphp.org/LICENSE.txt for license information
-// See http://polarphp.org/CONTRIBUTORS.txt for the list of POLARPHP_UTILS_SYSTEM_UTILS_H project authors
+// See http://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
 //
 // Created by softboy on 2018/07/15.
 
