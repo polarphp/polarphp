a160aa54ea1a786a4091a48de255dcd775e3864b
zzusoftboy <zzu_softboy@163.com>
2019-01-08 08:52:13 +0800

    add polarphp mock classes

--- tests/polarphp/polarphpmock/libpdkmock/StdlibExports.cpp
+++ tests/polarphp/polarphpmock/libpdkmock/StdlibExports.cpp
@@ -14,6 +14,10 @@
 #include "polarphp/vm/lang/Module.h"
 #include "ModuleCycleHooks.h"
 #include "Namespaces.h"
+#include "Constants.h"
+#include "Interfaces.h"
+#include "Functions.h"
+#include "Classes.h"
 #include "Inis.h"
 
 namespace php {
@@ -23,9 +27,15 @@ using polar::vmapi::Module;
 bool export_stdlib_to_zendvm()
 {
    static Module stdlibModule("stdlib");
+
    register_module_cycle_hooks(stdlibModule);
-   register_namepsace_hook(stdlibModule);
    register_inis_hook(stdlibModule);
+   register_namepsace_hook(stdlibModule);
+   register_constants_hook(stdlibModule);
+   register_interfaces_hook(stdlibModule);
+   register_functions_hook(stdlibModule);
+   register_classes_hook(stdlibModule);
+
    return stdlibModule.registerToVM();
 }
 
