b31594482a673d9a25800b8b21022b58255ad175
zzusoftboy <zzu_softboy@163.com>
2019-03-25 07:57:17 +0800

    add gyb.php

---
+++ devtools/gyb/README.md
@@ -0,0 +1,1 @@
+The php version of swift gyp tool
---
+++ devtools/gyb/gyb.php
@@ -0,0 +1,9 @@
+#!/usr/bin/env php
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+
+$application->run();
