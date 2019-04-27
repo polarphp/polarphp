e8c6ae9776c1814de18811b1ec269e0bd2ed6af3
zzusoftboy <zzu_softboy@163.com>
2019-04-27 11:21:58 +0800

    add AstNodesDefs

---
+++ include/polarphp/markup/Ast.h
@@ -0,0 +1,28 @@
+//===--- AST.h - Markup AST nodes -------------------------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2018 polarphp software foundation
+// Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://polarphp.org/LICENSE.txt for license information
+// See https://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2019/04/27.
+
+#ifndef POLARPHP_MARKUP_AST_H
+#define POLARPHP_MARKUP_AST_H
+
+
+
+#endif // POLARPHP_MARKUP_AST_H
