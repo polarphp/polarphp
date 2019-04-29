3716793275c85eda8ee3053591ce6719b9980ab4
zzusoftboy <zzu_softboy@163.com>
2019-04-29 22:59:29 +0800

    add NameLookup

---
+++ include/polarphp/ast/NameLookup.h
@@ -0,0 +1,477 @@
+//===--- NameLookup.h - Swift Name Lookup Routines --------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines interfaces for performing name lookup.
+//
+//===----------------------------------------------------------------------===//
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2018 polarphp software foundation
+// Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://polarphp.org/LICENSE.txt for license information
+// See https://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2019/04/29.
+
+#ifndef POLARPHP_AST_NAME_LOOKUP_H
+#define POLARPHP_AST_NAME_LOOKUP_H
+
+#include "polarphp/basic/adt/SmallPtrSet.h"
+#include "polarphp/basic/adt/SmallVector.h"
+#include "polarphp/ast/AstVisitor.h"
+#include "polarphp/ast/Identifier.h"
+#include "polarphp/parser/SourceLoc.h"
+#include "polarphp/parser/SourceMgr.h"
+
+namespace polar::ast {
+
+class ASTContext;
+class DeclContext;
+class DeclName;
+class Expr;
+class GenericSignatureBuilder;
+class LazyResolver;
+class TupleType;
+class Type;
+class TypeDecl;
+class ValueDecl;
+struct SelfBounds;
+
+using polar::parser::SourceManager;
+
+/// LookupResultEntry - One result of unqualified lookup.
+struct LookupResultEntry
+{
+private:
+
+   /// The declaration context through which we found Value. For instance,
+   /// class BaseClass {
+   ///   func foo() {}
+   /// }
+   ///
+   /// class DerivedClass : BaseClass {
+   ///   func bar() {}
+   /// }
+   ///
+   /// When finding foo() from the body of DerivedClass, BaseDC is DerivedClass.
+   ///
+   /// Another example:
+   ///
+   /// class BaseClass {
+   ///   func bar() {}
+   ///   func foo() {}
+   /// }
+   ///
+   /// When finding bar() from the function body of foo(), BaseDC is the method
+   /// foo().
+   DeclContext *BaseDC;
+
+   /// The declaration corresponds to the given name; i.e. the decl we are
+   /// looking up.
+   ValueDecl *Value;
+
+public:
+   LookupResultEntry(ValueDecl *value) : BaseDC(nullptr), Value(value)
+   {}
+
+   LookupResultEntry(DeclContext *baseDC, ValueDecl *value)
+      : BaseDC(baseDC), Value(value)
+   {}
+
+   ValueDecl *getValueDecl() const
+   {
+      return Value;
+   }
+
+   DeclContext *getDeclContext() const
+   {
+      return BaseDC;
+   }
+
+   ValueDecl *getBaseDecl() const;
+};
+
+/// This class implements and represents the result of performing
+/// unqualified lookup (i.e. lookup for a plain identifier).
+class UnqualifiedLookup
+{
+public:
+   enum class Flags
+   {
+      /// This lookup is known to not affect downstream files.
+      KnownPrivate = 0x01,
+      /// This lookup should only return types.
+      TypeLookup = 0x02,
+      /// This lookup should consider declarations within protocols to which the
+      /// context type conforms.
+      AllowProtocolMembers = 0x04,
+      /// Don't check access when doing lookup into a type.
+      IgnoreAccessControl = 0x08,
+      /// This lookup should include results from outside the innermost scope with
+      /// results.
+      IncludeOuterResults = 0x10,
+   };
+
+   using Options = OptionSet<Flags>;
+
+   /// Lookup an unqualified identifier \p Name in the context.
+   ///
+   /// If the current DeclContext is nested in a function body, the SourceLoc
+   /// is used to determine which declarations in that body are visible.
+   UnqualifiedLookup(DeclName Name, DeclContext *m_declContext, LazyResolver *TypeResolver,
+                     SourceLoc loc = SourceLoc(), Options options = Options());
+
+   SmallVector<LookupResultEntry, 4> Results;
+   /// The index of the first result that isn't from the innermost scope
+   /// with results.
+   ///
+   /// That is, \c makeArrayRef(Results).take_front(IndexOfFirstOuterResults)
+   /// will be Results from the innermost scope that had results, and the
+   /// remaining elements of Results will be from parent scopes of this one.
+   size_t IndexOfFirstOuterResult;
+
+   /// Return true if anything was found by the name lookup.
+   bool isSuccess() const
+   {
+      return !Results.empty();
+   }
+
+   /// Get the result as a single type, or a null type if that fails.
+   TypeDecl *getSingleTypeResult();
+};
+
+inline UnqualifiedLookup::Options operator|(UnqualifiedLookup::Flags flag1,
+                                            UnqualifiedLookup::Flags flag2)
+{
+   return UnqualifiedLookup::Options(flag1) | flag2;
+}
+
+/// Describes the reason why a certain declaration is visible.
+enum class DeclVisibilityKind
+{
+   /// Declaration is a local variable or type.
+   LocalVariable,
+
+   /// Declaration is a function parameter.
+   FunctionParameter,
+
+   /// Declaration is a generic parameter.
+   GenericParameter,
+
+   /// Declaration is a member of the immediately enclosing nominal decl.
+   ///
+   /// For example, 'Foo' is visible at (1) because of this.
+   /// \code
+   ///   struct A {
+   ///     typealias Foo = Int
+   ///     func f() {
+   ///       // (1)
+   ///     }
+   ///   }
+   /// \endcode
+   MemberOfCurrentNominal,
+
+   /// Declaration that is a requirement of a protocol implemented by the
+   /// immediately enclosing nominal decl, in case the nominal decl does not
+   /// supply a witness for this requirement.
+   ///
+   /// For example, 'foo' is visible at (1) because of this.
+   /// \code
+   /// protocol P {
+   ///   func foo()
+   /// }
+   /// struct A : P {
+   ///   func bar() {
+   ///     // (1)
+   ///   }
+   /// }
+   /// \endcode
+   MemberOfProtocolImplementedByCurrentNominal,
+
+   /// Declaration is a member of the superclass of the immediately enclosing
+   /// nominal decl.
+   MemberOfSuper,
+
+   /// Declaration is a member of the non-immediately enclosing nominal decl.
+   ///
+   /// For example, 'Foo' is visible at (1) because of this.
+   /// \code
+   ///   struct A {
+   ///     typealias Foo = Int
+   ///     struct B {
+   ///       func foo() {
+   ///         // (1)
+   ///       }
+   ///     }
+   ///   }
+   /// \endcode
+   MemberOfOutsideNominal,
+
+   /// Declaration is visible at the top level because it is declared in this
+   /// module or in an imported module.
+   VisibleAtTopLevel,
+
+   /// Declaration was found via \c AnyObject or \c AnyObject.Type.
+   DynamicLookup,
+};
+
+/// An abstract base class for a visitor that consumes declarations found within
+/// a given context.
+class VisibleDeclConsumer
+{
+public:
+   virtual ~VisibleDeclConsumer() = default;
+
+   /// This method is called by findVisibleDecls() every time it finds a decl.
+   virtual void foundDecl(ValueDecl *foundDecl, DeclVisibilityKind reason) = 0;
+
+private:
+   virtual void anchor();
+};
+
+/// An implementation of VisibleDeclConsumer that's built from a lambda.
+template <class FuncType>
+class LambdaDeclConsumer : public VisibleDeclConsumer
+{
+public:
+   LambdaDeclConsumer(FuncType &&callback)
+      : m_callback(std::move(callback))
+   {}
+
+   void foundDecl(ValueDecl *valueDecl, DeclVisibilityKind reason)
+   {
+      m_callback(valueDecl, reason);
+   }
+
+private:
+   FuncType m_callback;
+};
+
+template <class FuncType>
+LambdaDeclConsumer<FuncType> make_decl_consumer(FuncType &&callback)
+{
+   return LambdaDeclConsumer<FuncType>(std::move(callback));
+}
+
+/// A consumer that inserts found decls into an externally-owned SmallVector.
+class VectorDeclConsumer : public VisibleDeclConsumer
+{
+public:
+   SmallVectorImpl<ValueDecl *> &results;
+   explicit VectorDeclConsumer(SmallVectorImpl<ValueDecl *> &decls)
+      : results(decls)
+   {}
+
+   virtual void foundDecl(ValueDecl *valueDecl, DeclVisibilityKind reason) override
+   {
+      results.push_back(valueDecl);
+   }
+private:
+   virtual void anchor() override;
+};
+
+/// A consumer that inserts found decls with a matching name into an
+/// externally-owned SmallVector.
+class NamedDeclConsumer : public VisibleDeclConsumer
+{
+   virtual void anchor() override;
+public:
+   DeclName name;
+   SmallVectorImpl<LookupResultEntry> &results;
+   bool isTypeLookup;
+
+   NamedDeclConsumer(DeclName name,
+                     SmallVectorImpl<LookupResultEntry> &results,
+                     bool isTypeLookup)
+      : name(name),
+        results(results),
+        isTypeLookup(isTypeLookup)
+   {}
+
+   virtual void foundDecl(ValueDecl *valueDecl, DeclVisibilityKind reason) override
+   {
+      // Give clients an opportunity to filter out non-type declarations early,
+      // to avoid circular validation.
+      if (isTypeLookup && !isa<TypeDecl>(valueDecl)) {
+         return;
+      }
+      if (valueDecl->getFullName().matchesRef(name)) {
+         results.push_back(LookupResultEntry(valueDecl));
+      }
+   }
+};
+
+/// A consumer that filters out decls that are not accessible from a given
+/// DeclContext.
+class AccessFilteringDeclConsumer final : public VisibleDeclConsumer
+{
+public:
+   AccessFilteringDeclConsumer(const DeclContext *declContext,
+                               VisibleDeclConsumer &consumer)
+      : m_declContext(declContext),
+        m_chainedConsumer(consumer)
+   {}
+
+   void foundDecl(ValueDecl *valueDecl, DeclVisibilityKind reason) override;
+private:
+   const DeclContext *m_declContext;
+   VisibleDeclConsumer &m_chainedConsumer;
+};
+
+/// Remove any declarations in the given set that were overridden by
+/// other declarations in that set.
+///
+/// \returns true if any declarations were removed, false otherwise.
+bool remove_overridden_decls(SmallVectorImpl<ValueDecl*> &decls);
+
+/// Remove any declarations in the given set that are shadowed by
+/// other declarations in that set.
+///
+/// \param decls The set of declarations being considered.
+/// \param curModule The current module.
+///
+/// \returns true if any shadowed declarations were removed.
+bool remove_shadowed_decls(SmallVectorImpl<ValueDecl*> &decls,
+                           const ModuleDecl *curModule);
+
+/// Finds decls visible in the given context and feeds them to the given
+/// VisibleDeclConsumer.  If the current DeclContext is nested in a function,
+/// the SourceLoc is used to determine which declarations in that function
+/// are visible.
+void lookup_visible_decls(VisibleDeclConsumer &consumer,
+                          const DeclContext *declContext,
+                          LazyResolver *typeResolver,
+                          bool includeTopLevel,
+                          SourceLoc loc = SourceLoc());
+
+/// Finds decls visible as members of the given type and feeds them to the given
+/// VisibleDeclConsumer.
+///
+/// \param currDeclContext the DeclContext from which the lookup is done.
+void lookup_visible_member_decls(VisibleDeclConsumer &consumer,
+                                 Type baseType,
+                                 const DeclContext *currDeclContext,
+                                 LazyResolver *typeResolver,
+                                 bool includeInstanceMembers,
+                                 GenericSignatureBuilder *gsb = nullptr);
+
+/// Retrieve the set of nominal type declarations that are directly
+/// "inherited" by the given declaration at a particular position in the
+/// list of "inherited" types.
+///
+/// Add anything we find to the \c result vector. If we come across the
+/// AnyObject type, set \c anyObject true.
+void get_directly_inherited_nominal_type_decls(
+      PointerUnion<TypeDecl *, ExtensionDecl *> decl,
+      unsigned i,
+      SmallVectorImpl<std::pair<SourceLoc, NominalTypeDecl *>> &result,
+      bool &anyObject);
+
+/// Retrieve the set of nominal type declarations that are directly
+/// "inherited" by the given declaration, looking through typealiases
+/// and splitting out the components of compositions.
+///
+/// If we come across the AnyObject type, set \c anyObject true.
+SmallVector<std::pair<SourceLoc, NominalTypeDecl *>, 4>
+get_directly_inherited_nominal_type_decls(
+      PointerUnion<TypeDecl *, ExtensionDecl *> decl,
+      bool &anyObject);
+
+/// Retrieve the set of nominal type declarations that appear as the
+/// constraint type of any "Self" constraints in the where clause of the
+/// given protocol or protocol extension.
+SelfBounds get_self_bounds_from_where_clause(
+      PointerUnion<TypeDecl *, ExtensionDecl *> decl);
+
+namespace namelookup {
+
+/// Searches through statements and patterns for local variable declarations.
+class FindLocalVal : public StmtVisitor<FindLocalVal>
+{
+public:
+   FindLocalVal(const SourceManager &sourceMgr, SourceLoc loc,
+                VisibleDeclConsumer &consumer)
+      : m_sm(sourceMgr),
+        m_loc(loc),
+        m_consumer(consumer)
+   {}
+
+   void checkValueDecl(ValueDecl *valueDecl, DeclVisibilityKind reason)
+   {
+      m_consumer.foundDecl(valueDecl, reason);
+   }
+
+   void checkParameterList(const ParameterList *params);
+   void checkGenericParams(GenericParamList *params);
+   void checkSourceFile(const SourceFile &sourceFile);
+
+private:
+   bool isReferencePointInRange(SourceRange range)
+   {
+      return m_sm.rangeContainsTokenLoc(range, loc);
+   }
+
+   void visitBreakStmt(BreakStmt *)
+   {}
+
+   void visitContinueStmt(ContinueStmt *)
+   {}
+
+   void visitFallthroughStmt(FallthroughStmt *)
+   {}
+
+   void visitFailStmt(FailStmt *)
+   {}
+
+   void visitReturnStmt(ReturnStmt *)
+   {}
+
+   void visitYieldStmt(YieldStmt *)
+   {}
+
+   void visitThrowStmt(ThrowStmt *)
+   {}
+
+   void visitPoundAssertStmt(PoundAssertStmt *)
+   {}
+   void visitDeferStmt(DeferStmt *deferStmt)
+   {
+      // Nothing in the defer is visible.
+   }
+
+   void checkStmtCondition(const StmtCondition &cond);
+   void visitIfStmt(IfStmt *stmt);
+   void visitGuardStmt(GuardStmt *stmt);
+   void visitWhileStmt(WhileStmt *stmt);
+   void visitRepeatWhileStmt(RepeatWhileStmt *stmt);
+   void visitDoStmt(DoStmt *stmt);
+   void visitForEachStmt(ForEachStmt *stmt);
+   void visitBraceStmt(BraceStmt *stmt, bool isTopLevelCode = false);
+   void visitSwitchStmt(SwitchStmt *stmt);
+   void visitCaseStmt(CaseStmt *stmt);
+   void visitDoCatchStmt(DoCatchStmt *stmt);
+   void visitCatchClauses(ArrayRef<CatchStmt*> clauses);
+   void visitCatchStmt(CatchStmt *stmt);
+
+private:
+   friend class AstVisitor<FindLocalVal>;
+   const SourceManager &m_sm;
+   SourceLoc m_loc;
+   VisibleDeclConsumer &m_consumer;
+};
+
+} // namelookup
+} // polar::ast
+
+#endif // POLARPHP_AST_NAME_LOOKUP_H
