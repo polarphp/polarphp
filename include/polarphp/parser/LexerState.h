6c1ff4077ac3d817f125a30cd7d5d6311e104c7e
zzusoftboy <zzu_softboy@163.com>
2019-06-05 22:00:03 +0800

    add polar::parser::LexerState

---
+++ include/polarphp/parser/LexerState.h
@@ -0,0 +1,69 @@
+//===--- LexerState.h - Lexer State -----------------------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file defines the LexerState object.
+//
+//===----------------------------------------------------------------------===//
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2019 polarphp software foundation
+// Copyright (c) 2017 - 2019 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://polarphp.org/LICENSE.txt for license information
+// See https://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2019/06/05.
+
+#ifndef POLARPHP_PARSER_PARSED_LEXER_STATE_H
+#define POLARPHP_PARSER_PARSED_LEXER_STATE_H
+
+#include <optional>
+#include "polarphp/parser/SourceLoc.h"
+#include "polarphp/parser/ParsedTrivia.h"
+
+namespace polar::parser {
+
+class Lexer;
+
+/// Lexer state can be saved/restored to/from objects of this class.
+
+class LexerState
+{
+public:
+   LexerState()
+   {}
+
+   bool isValid() const
+   {
+      return m_loc.isValid();
+   }
+
+   LexerState advance(unsigned offset) const
+   {
+      assert(isValid());
+      return LexerState(m_loc.getAdvancedLoc(offset));
+   }
+
+private:
+   explicit LexerState(SourceLoc loc)
+      : m_loc(loc)
+   {}
+
+   SourceLoc m_loc;
+   std::optional<ParsedTrivia> m_leadingTrivia;
+   friend class Lexer;
+};
+
+} // polar::parser
+
+#endif // POLARPHP_PARSER_PARSED_LEXER_STATE_H
