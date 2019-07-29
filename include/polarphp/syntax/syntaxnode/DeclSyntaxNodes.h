c3cbebf9ee060cec560ca4f60efd596488557d66
zzusoftboy <zzu_softboy@163.com>
2019-07-29 09:52:04 +0800

    add ReservedNonModifierSyntax interface

--- include/polarphp/syntax/syntaxnode/DeclSyntaxNodes.h
+++ include/polarphp/syntax/syntaxnode/DeclSyntaxNodes.h
@@ -21,6 +21,43 @@
 namespace polar::syntax {
 
 class SourceFileSyntax;
+class ReservedNonModifierSyntax;
+
+class ReservedNonModifierSyntax : public Syntax
+{
+public:
+   constexpr static unsigned int CHILDREN_COUNT = 1;
+   constexpr static unsigned int REQUIRED_CHILDREN_COUNT = 1;
+
+   enum Cursor : SyntaxChildrenCountType
+   {
+      /// type: TokenSyntax
+      /// optional: false
+      /// token choices:
+      /// T_INCLUDE | T_INCLUDE_ONCE | T_EVAL | T_REQUIRE | T_REQUIRE_ONCE | T_LOGICAL_OR | T_LOGICAL_XOR | T_LOGICAL_AND
+      /// T_INSTANCEOF | T_NEW | T_CLONE | T_EXIT | T_IF | T_ELSEIF | T_ELSE | T_ECHO | T_DO | T_WHILE
+      /// T_FOR | T_FOREACH | T_DECLARE | T_AS | T_TRY | T_CATCH | T_FINALLY
+      /// T_THROW | T_USE | T_INSTEADOF | T_GLOBAL | T_VAR | T_UNSET | T_ISSET | T_EMPTY | T_CONTINUE | T_GOTO
+      /// T_FUNCTION | T_CONST | T_RETURN | T_PRINT | T_YIELD | T_LIST | T_SWITCH | T_CASE | T_DEFAULT | T_BREAK
+      /// T_ARRAY | T_CALLABLE | T_EXTENDS | T_IMPLEMENTS | T_NAMESPACE | T_TRAIT | T_INTERFACE | T_CLASS
+      /// T_CLASS_CONST | T_TRAIT_CONST | T_FUNC_CONST | T_METHOD_CONST | T_LINE | T_FILE | T_DIR | T_NS_CONST | T_FN
+      Modifier
+   };
+
+public:
+   ReservedNonModifierSyntax(const RefCountPtr<SyntaxData> root, const SyntaxData *data)
+      : Syntax(root, data)
+   {
+      validate();
+   }
+
+   TokenSyntax getModifier();
+   ReservedNonModifierSyntax with(std::optional<TokenSyntax> modifier);
+
+private:
+   friend class ReservedNonModifierSyntaxBuilder;
+   void validate();
+};
 
 class SourceFileSyntax : public Syntax
 {
