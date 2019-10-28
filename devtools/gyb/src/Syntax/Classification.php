8ce0da006d5c75546df759de5c623f68c3aec1ae
zzusoftboy <zzu_softboy@163.com>
2019-10-28 23:00:42 +0800

    add gyb Token class

--- devtools/gyb/src/Syntax/Classification.php
+++ devtools/gyb/src/Syntax/Classification.php
@@ -1,1 +1,127 @@
 <?php
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2019 polarphp software foundation
+// Copyright (c) 2017 - 2019 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://polarphp.org/LICENSE.txt for license information
+// See https://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2019/10/28.
+
+namespace Gyb\Syntax;
+
+/**
+ * Represents a classification a token can receive for syntax highlighting.
+ *
+ * @package Gyb\Syntax
+ */
+class SyntaxClassification
+{
+   /**
+    * @var string $name
+    */
+   private $name;
+   /**
+    * @var string $polarName
+    */
+   private $polarName;
+   /**
+    * @var string $description
+    */
+   private $description;
+
+   /**
+    * @var array $pool
+    */
+   private static $pool = array();
+
+   public function __construct(string $name, string $polarName, string $description)
+   {
+      $this->name = $name;
+      $this->polarName = $polarName;
+      $this->description = $description;
+   }
+
+   static function getByName(string $name)
+   {
+      if (empty(self::$pool)) {
+         self::initPool();
+      }
+      if (array_key_exists($name)) {
+         return self::$pool[$name];
+      }
+      throw new \RuntimeException(sprintf("Unknown syntax classification '%s'", $name));
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getName()
+   {
+      return $this->name;
+   }
+
+   /**
+    * @param mixed $name
+    * @return SyntaxClassification
+    */
+   public function setName($name)
+   {
+      $this->name = $name;
+      return $this;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getPolarName()
+   {
+      return $this->polarName;
+   }
+
+   /**
+    * @param mixed $polarName
+    * @return SyntaxClassification
+    */
+   public function setPolarName($polarName)
+   {
+      $this->polarName = $polarName;
+      return $this;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getDescription()
+   {
+      return $this->description;
+   }
+
+   /**
+    * @param mixed $description
+    * @return SyntaxClassification
+    */
+   public function setDescription($description)
+   {
+      $this->description = $description;
+      return $this;
+   }
+
+   protected static function initPool(): void
+   {
+      self::$pool = [
+         new SyntaxClassification('None', '', 'The token should not receive syntax coloring.'),
+         new SyntaxClassification('Keyword', '', 'A polarphp keyword, including contextual keywords.'),
+         new SyntaxClassification('Identifier', '', 'A generic identifier.'),
+         new SyntaxClassification('IntegerLiteral', '', 'An integer literal.'),
+         new SyntaxClassification('FloatingLiteral', '', 'A floating point literal.'),
+         new SyntaxClassification('StringLiteral', '', 'A string literal including multiline string literals.'),
+         new SyntaxClassification('LineComment', '', 'A line comment starting with `//`.'),
+         new SyntaxClassification('DocLineComment','', 'A doc line comment starting with `///`.'),
+         new SyntaxClassification('BlockComment', '', 'A block comment starting with `/**` and ending with `*/.'),
+         new SyntaxClassification('DocBlockComment', '', 'A doc block comment starting with `/**` and ending with `*/.')
+      ];
+   }
+}
--- devtools/gyb/src/Syntax/Token.php
+++ devtools/gyb/src/Syntax/Token.php
@@ -1,1 +1,203 @@
 <?php
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2019 polarphp software foundation
+// Copyright (c) 2017 - 2019 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://polarphp.org/LICENSE.txt for license information
+// See https://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2019/10/28.
+
+namespace Gyb\Syntax;
+
+/**
+ * Represents the specification for a Token in the TokenSyntax file.
+ *
+ * Class Token
+ * @package Gyb\Syntax
+ */
+class Token
+{
+   /**
+    * @var string $name
+    */
+   private $name;
+   /**
+    * @var string $kind
+    */
+   private $kind;
+   /**
+    * @var string $unprefixedKind
+    */
+   private $unprefixedKind;
+   /**
+    * @var string $serializationCode
+    */
+   private $serializationCode;
+   /**
+    * @var string $text
+    */
+   private $text;
+   /**
+    * @var string $classification
+    */
+   private $classification;
+   /**
+    * @var bool $isKeyword
+    */
+   private $isKeyword;
+
+   public function __construct(string $name, string $kind, $serializationCode = '',
+                               string $unprefixedKind = '', string $text = '',
+                               string $classification = '', bool $isKeyword = false)
+   {
+      $this->name = trim($name);
+      $this->kind = trim($kind);
+      if ($unprefixedKind === null) {
+         $this->unprefixedKind = $kind;
+      } else {
+         $this->unprefixedKind = $unprefixedKind;
+      }
+      $this->serializationCode = $serializationCode;
+      $this->text = $text;
+      $this->classification = SyntaxClassification::getByName(trim($classification));
+      $this->isKeyword = $isKeyword;
+   }
+
+   public function getPolarKind(): string
+   {
+      $name = lcfirst($this->name);
+      if ($this->isKeyword) {
+         return $name . 'Keyword';
+      }
+      return $name;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getName()
+   {
+      return $this->name;
+   }
+
+   /**
+    * @param mixed $name
+    * @return Token
+    */
+   public function setName($name)
+   {
+      $this->name = $name;
+      return $this;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getKind()
+   {
+      return $this->kind;
+   }
+
+   /**
+    * @param mixed $kind
+    * @return Token
+    */
+   public function setKind($kind)
+   {
+      $this->kind = $kind;
+      return $this;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getUnprefixedKind()
+   {
+      return $this->unprefixedKind;
+   }
+
+   /**
+    * @param mixed $unprefixedKind
+    * @return Token
+    */
+   public function setUnprefixedKind($unprefixedKind)
+   {
+      $this->unprefixedKind = $unprefixedKind;
+      return $this;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getSerializationCode()
+   {
+      return $this->serializationCode;
+   }
+
+   /**
+    * @param mixed $serializationCode
+    * @return Token
+    */
+   public function setSerializationCode($serializationCode)
+   {
+      $this->serializationCode = $serializationCode;
+      return $this;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getText()
+   {
+      return $this->text;
+   }
+
+   /**
+    * @param mixed $text
+    * @return Token
+    */
+   public function setText($text)
+   {
+      $this->text = $text;
+      return $this;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getClassification()
+   {
+      return $this->classification;
+   }
+
+   /**
+    * @param mixed $classification
+    * @return Token
+    */
+   public function setClassification($classification)
+   {
+      $this->classification = $classification;
+      return $this;
+   }
+
+   /**
+    * @return mixed
+    */
+   public function getIsKeyword()
+   {
+      return $this->isKeyword;
+   }
+
+   /**
+    * @param mixed $isKeyword
+    * @return Token
+    */
+   public function setIsKeyword($isKeyword)
+   {
+      $this->isKeyword = $isKeyword;
+      return $this;
+   }
+}
