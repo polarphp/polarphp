8dba568329ebc6d805a587d7744bb5de29db0a13
zzusoftboy <zzu_softboy@163.com>
2019-11-29 13:43:09 +0800

    add StlExtras

---
+++ include/polarphp/ast/PrintOptions.h
@@ -0,0 +1,34 @@
+//===--- PrintOptions.h - AST printing options ------------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//===----------------------------------------------------------------------===//
+// This source file is part of the polarphp.org open source project
+// Copyright (c) 2017 - 2019 polarphp software foundation
+// Copyright (c) 2017 - 2019 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://polarphp.org/LICENSE.txt for license information
+// See https://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2019/11/28.
+//
+
+#ifndef POLARPHP_AST_PRINT_OPTIONS_H
+#define POLARPHP_AST_PRINT_OPTIONS_H
+
+#include "polarphp/basic/StlExtras.h"
+#include "polarphp/ast/AttrKind.h"
+#include "polarphp/syntax/TokenSyntax.h"
+#include "llvm/ADT/Optional.h"
+#include "llvm/ADT/DenseMap.h"
+#include <limits.h>
+#include <vector>
+
+
+#endif // POLARPHP_AST_PRINT_OPTIONS_H
