fdfe7a8a2888c5bd889cda1f70a04aaa3c1a84a3
zzusoftboy <zzu_softboy@163.com>
2019-12-01 16:49:04 +0800

    add PrettyStackTrace class

---
+++ include/polarphp/basic/PrettyStackTrace.h
@@ -0,0 +1,67 @@
+//===--- PrettyStackTrace.h - Generic stack-trace prettifiers ---*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2019 polarphp software foundation
+// Copyright (c) 2017 - 2019 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://polarphp.org/LICENSE.txt for license information
+// See https://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2019/12/01.
+
+#ifndef POLARPHP_BASIC_PRETTY_STACKTRACE_H
+#define POLARPHP_BASIC_PRETTY_STACKTRACE_H
+
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/ADT/StringRef.h"
+
+namespace llvm {
+class MemoryBuffer;
+}
+
+namespace polar::basic {
+
+/// A PrettyStackTraceEntry for performing an action involving a StringRef.
+///
+/// The message is:
+///   While <action> "<string>"\n
+class PrettyStackTraceStringAction : public llvm::PrettyStackTraceEntry
+{
+   const char *m_action;
+   llvm::StringRef m_theString;
+public:
+   PrettyStackTraceStringAction(const char *action, llvm::StringRef string)
+      : m_action(action),
+        m_theString(string)
+   {}
+
+   void print(llvm::raw_ostream &ostream) const override;
+};
+
+/// A PrettyStackTraceEntry to dump the contents of a file.
+class PrettyStackTraceFileContents : public llvm::PrettyStackTraceEntry
+{
+   const llvm::MemoryBuffer &m_buffer;
+public:
+   explicit PrettyStackTraceFileContents(const llvm::MemoryBuffer &buffer)
+      : m_buffer(buffer)
+   {}
+
+   void print(llvm::raw_ostream &ostream) const override;
+};
+
+
+} // polar::basic
+
+#endif // POLARPHP_BASIC_PRETTY_STACKTRACE_H
---
+++ src/basic/PrettyStackTrace.cpp
@@ -0,0 +1,50 @@
+//===--- PrettyStackTrace.cpp - Generic PrettyStackTraceEntries -----------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+// This source file is part of the polarphp.org open source project
+//
+// Copyright (c) 2017 - 2019 polarphp software foundation
+// Copyright (c) 2017 - 2019 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://polarphp.org/LICENSE.txt for license information
+// See https://polarphp.org/CONTRIBUTORS.txt for the list of polarphp project authors
+//
+// Created by polarboy on 2019/12/01.
+//===----------------------------------------------------------------------===//
+//
+//  This file implements several PrettyStackTraceEntries that probably
+//  ought to be in LLVM.
+//
+//===----------------------------------------------------------------------===//
+
+#include "polarphp/basic/PrettyStackTrace.h"
+#include "polarphp/basic/QuotedString.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace polar::basic {
+
+void PrettyStackTraceStringAction::print(llvm::raw_ostream &out) const
+{
+   out << "While " << m_action << ' ' << QuotedString(m_theString) << '\n';
+}
+
+void PrettyStackTraceFileContents::print(llvm::raw_ostream &out) const
+{
+   out << "Contents of " << m_buffer.getBufferIdentifier() << ":\n---\n"
+       << m_buffer.getBuffer();
+   if (!m_buffer.getBuffer().endswith("\n")) {
+      out << '\n';
+   }
+   out << "---\n";
+}
+
+} // polar::basic
